3b6d332bc9d0159b0a64ac535fa8ef5654c8853a
HVboddeti <harsha090500@gmail.com>
2025-02-27 19:23:42 -0600

    api chnage

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ function App() {
         formData.append("job_description", jobDescription);
 
         try {
-            const response = await fetch("https://warfu0feja.execute-api.us-east-1.amazonaws.com/Dev/resume-processing-lambda", {
+            const response = await fetch("https://b1qyocwv91.execute-api.us-east-1.amazonaws.com/start/resume-processing-lambda", {
                 method: "POST",
                 body: formData
             });

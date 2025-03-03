13c727e96b8dfd06f89f0f3529442dc719a70646
HVboddeti <harsha090500@gmail.com>
2025-03-02 19:41:29 -0600

    tex

--- src/components/ResumeUpload.js
+++ src/components/ResumeUpload.js
@@ -4,7 +4,7 @@ function ResumeUpload({ setResume }) {
     return (
         <div style={{ marginBottom: "20px" }}>
             <h3>Upload Resume (PDF/DOCX)</h3>
-            <input type="file" accept=".pdf,.docx" onChange={(e) => setResume(e.target.files[0])} />
+            <input type="file" accept=".tex" onChange={(e) => setResume(e.target.files[0])} />
         </div>
     );
 }
